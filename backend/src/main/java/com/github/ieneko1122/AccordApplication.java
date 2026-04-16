ba1bdcdb9d008be88ea631726877bddd23ccfd2a
ieneko1122 <keisuke.iemura@gmail.com>
2026-04-16 17:01:13 +0900

    chore: add Spring Boot backend and Vue 3 frontend

---
+++ backend/src/main/java/com/github/ieneko1122/AccordApplication.java
@@ -0,0 +1,13 @@
+package com.github.ieneko1122;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccordApplication.class, args);
+	}
+
+}
